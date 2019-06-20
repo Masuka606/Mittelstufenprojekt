de9b3b354edff256c5c9aff6e15a82f69d3bb189
Masuka606 <masuka606@gmail.com>
2019-06-20 15:32:41 +0200

    neue Seiten mit Buttons

---
+++ Musikschule/Pages/Administration.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Musikschule.Pages
+{
+    public class AdministrationModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Bitte wählen Sie eine der folgenden Funktionen aus.";
+        }
+    }
+}
